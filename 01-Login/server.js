59fa1ea9508bba0616d48a9509031c81323a4d03
kikeyama <20974893+kikeyama@users.noreply.github.com>
2023-12-14 19:50:47 +0900

    initial commit with backchannel logout added

---
+++ 01-Login/server.js
@@ -0,0 +1,72 @@
+require('dotenv').config({ path: `${__dirname}/.env` });
+const express = require('express');
+const http = require('http');
+const logger = require('morgan');
+const path = require('path');
+const router = require('./routes/index');
+const { auth } = require('express-openid-connect');
+const { createClient } = require('redis');
+const RedisStore = require('connect-redis')(auth);
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  idpLogout: true,
+};
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+// Back-Channel Logout
+let redisClient = createClient({
+  url: process.env.REDIS_URL,
+  legacyMode: true,
+});
+redisClient.connect().catch(console.error);
+
+config.backchannelLogout = true;
+config.session = {
+  store: new RedisStore({ client: redisClient }),
+};
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
+
+app.use('/', router);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handlers
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: process.env.NODE_ENV !== 'production' ? err : {}
+  });
+});
+
+http.createServer(app)
+  .listen(port, () => {
+    console.log(`Listening on ${config.baseURL}`);
+  });
